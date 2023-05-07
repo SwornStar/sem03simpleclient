d87e94b683705d3fa1db3dbec40d4dbd79630b87
SwornStar <nhngo@uia.no>
2023-05-07 16:22:08 +0000

    Last

--- tcp_client.go
+++ tcp_client.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"net"
 	"log"
+	"net"
 	"os"
+	
+	"github.com/SwornStar/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.3:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
+
 	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +26,8 @@ func main() {
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
-	response := string(buf[:n])
-	log.Printf("reply from proxy: %s", response)
+	}
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	response := string(dekryptertMelding)
+	log.Printf("Svar fra serveren: %s", response)
 }
